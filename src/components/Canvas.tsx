0b4be175ebf5dc24d8d031a6f460e7d73b81eb14
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2020-11-21 17:57:47 +0200

    Feature: Adding basic Canvas component
    Utilises the canvas hook

---
+++ src/components/Canvas.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import useCanvas from "../hooks/useCanvas";
+
+type CanvasProps = {
+  draw: (context: CanvasRenderingContext2D) => void;
+};
+
+const Canvas = ({ draw, ...rest }: CanvasProps) => {
+  const canvasRef = useCanvas(draw);
+
+  return <canvas ref={canvasRef} {...rest} />;
+};
+
+export default Canvas;
