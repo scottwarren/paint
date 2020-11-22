e98e04690bf15780d0d51147524ba31da8829a7c
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2020-11-22 14:50:08 +0200

    Feature: Started tracking "drawing" movements when the mouse is clicked and held

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,15 +1,92 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
-import useCanvas from '../hooks/useCanvas';
+const LINE_WIDTH = 2;
 
-type CanvasProps = {
-  draw: (context: CanvasRenderingContext2D) => void;
-};
+// TODO: Get colour from pallete
+const STROKE_STYLE = '#000000';
 
-function Canvas({ draw, ...rest }: CanvasProps): React.ReactElement {
-  const canvasRef = useCanvas(draw);
+function Canvas(): React.ReactElement {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  let isMouseDown = false;
 
-  return <canvas ref={canvasRef} {...rest} />;
+  let context: CanvasRenderingContext2D;
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+
+    if (!canvas) {
+      console.log('...waiting for canvas...');
+      return;
+    }
+
+    context = canvas.getContext('2d') as CanvasRenderingContext2D;
+  }, [canvasRef]);
+
+  const handleMouseDown = (ev: React.MouseEvent) => {
+    // get the mouse co-ordinates to know where the "start" is of the drawing
+    const { clientX, clientY } = ev;
+
+    console.log('clientX', clientX);
+    console.log('clientY', clientY);
+
+    if (!context) {
+      return;
+    }
+
+    isMouseDown = true;
+
+    context.beginPath();
+    context.lineWidth = LINE_WIDTH;
+    context.strokeStyle = STROKE_STYLE;
+
+    context.moveTo(clientX, clientY);
+
+    return true;
+  };
+
+  const handleMouseMove = (ev: React.MouseEvent) => {
+    if (!isMouseDown) {
+      return;
+    }
+
+    const { clientX, clientY } = ev;
+    console.log('clientX', clientX);
+    console.log('clientY', clientY);
+
+    if (!context) {
+      return;
+    }
+
+    context.beginPath();
+    context.lineWidth = LINE_WIDTH;
+    context.strokeStyle = STROKE_STYLE;
+
+    context.moveTo(clientX, clientY);
+  };
+
+  const handleMouseUp = (ev: React.MouseEvent) => {
+    isMouseDown = false;
+
+    const { clientX, clientY } = ev;
+    console.log('clientX', clientX);
+    console.log('clientY', clientY);
+    if (!context) {
+      return;
+    }
+    // finish path?
+    context.lineTo(clientX, clientY);
+    context.stroke();
+  };
+
+  return (
+    <canvas
+      ref={canvasRef}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+      style={{ width: '100vw' }}
+    />
+  );
 }
 
 export default Canvas;
--- src/pages/Paint.tsx
+++ src/pages/Paint.tsx
@@ -2,17 +2,8 @@ import React from 'react';
 
 import Canvas from '../components/Canvas';
 
-function draw(ctx: CanvasRenderingContext2D): void {
-  ctx.fillStyle = '#000000';
-
-  ctx.beginPath();
-
-  ctx.arc(50, 100, 20, 0, 2 * Math.PI);
-  ctx.fill();
-}
-
 function Paint(): React.ReactElement {
-  return <Canvas draw={draw} />;
+  return <Canvas />;
 }
 
 export default Paint;
