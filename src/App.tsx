4073a630abd715c3d36ddd4948d0243b373211ac
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2020-11-22 16:17:57 +0200

    Feature: Adding basic drawing (using rectangles)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import GlobalStyles from './global-styles';
+
 import Routes from './Routes';
 import Navigation from './layout/Navigation';
 
 function App(): React.ReactElement {
   return (
-    <Router>
-      <div>
-        <Navigation />
-        <Routes />
-      </div>
-    </Router>
+    <>
+      <GlobalStyles />
+      <Router>
+        <div>
+          <Navigation />
+          <Routes />
+        </div>
+      </Router>
+    </>
   );
 }
 
--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,47 +1,55 @@
 import React, { useEffect, useRef } from 'react';
+import styled from 'styled-components';
 
-const LINE_WIDTH = 2;
+const PaintingCanvas = styled.canvas`
+  width: 100vw;
+  // TODO: make it fit height of container
+  height: 800px;
+`;
 
-// TODO: Get colour from pallete
-const STROKE_STYLE = '#000000';
+const LINE_SIZE = 2;
+
+// // TODO: Get colour from pallete
+// const STROKE_STYLE = '#000000';
+
+function getMousePos(canvas: HTMLCanvasElement, ev: React.MouseEvent) {
+  const rect = canvas.getBoundingClientRect();
+
+  return {
+    x: ev.clientX - rect.left,
+    y: ev.clientY - rect.top,
+  };
+}
 
 function Canvas(): React.ReactElement {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   let isMouseDown = false;
 
+  let canvas: HTMLCanvasElement | null;
   let context: CanvasRenderingContext2D;
-
   useEffect(() => {
-    const canvas = canvasRef.current;
-
+    canvas = canvasRef.current;
     if (!canvas) {
       console.log('...waiting for canvas...');
       return;
     }
 
+    // Setting the Canvas height/width so it scales appopriately and doesn't reset to default (300px)
+    // Required because I'm using a non-fixed width
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+
     context = canvas.getContext('2d') as CanvasRenderingContext2D;
   }, [canvasRef]);
 
-  const handleMouseDown = (ev: React.MouseEvent) => {
-    // get the mouse co-ordinates to know where the "start" is of the drawing
-    const { clientX, clientY } = ev;
-
-    console.log('clientX', clientX);
-    console.log('clientY', clientY);
-
+  // const handleMouseDown = (ev: React.MouseEvent) => {
+  const handleMouseDown = () => {
     if (!context) {
+      console.error('no context yet...?');
       return;
     }
 
     isMouseDown = true;
-
-    context.beginPath();
-    context.lineWidth = LINE_WIDTH;
-    context.strokeStyle = STROKE_STYLE;
-
-    context.moveTo(clientX, clientY);
-
-    return true;
   };
 
   const handleMouseMove = (ev: React.MouseEvent) => {
@@ -49,42 +57,60 @@ function Canvas(): React.ReactElement {
       return;
     }
 
-    const { clientX, clientY } = ev;
-    console.log('clientX', clientX);
-    console.log('clientY', clientY);
-
     if (!context) {
+      console.error('no context yet...?');
       return;
     }
+
+    if (!canvas) {
+      console.error('no canvas yet...');
+      return;
+    }
+
+    const { x, y } = getMousePos(canvas, ev);
 
     context.beginPath();
-    context.lineWidth = LINE_WIDTH;
-    context.strokeStyle = STROKE_STYLE;
+    // context.moveTo(0, 0);
+    context.lineTo(x, y);
+    context.stroke();
 
-    context.moveTo(clientX, clientY);
+    context.rect(x, y, LINE_SIZE, LINE_SIZE);
+    context.fill();
+
+    context.closePath();
+
+    // x = newX;
+    // y = newY;
   };
 
-  const handleMouseUp = (ev: React.MouseEvent) => {
+  const handleMouseUp = () => {
+    // const handleMouseUp = (ev: React.MouseEvent) => {
     isMouseDown = false;
 
-    const { clientX, clientY } = ev;
-    console.log('clientX', clientX);
-    console.log('clientY', clientY);
-    if (!context) {
-      return;
-    }
-    // finish path?
-    context.lineTo(clientX, clientY);
-    context.stroke();
+    // const { clientX, clientY } = ev;
+    // console.log('clientX', clientX);
+    // console.log('clientY', clientY);
+
+    // if (!context) {
+    //   console.error('no context yet...?');
+    //   return;
+    // }
+    // context.beginPath();
+    // context.moveTo(50, 50);
+    // context.lineTo(50, 50);
+    // context.stroke();
+
+    // // finish path?
+
+    // context.closePath();
   };
 
   return (
-    <canvas
+    <PaintingCanvas
       ref={canvasRef}
       onMouseDown={handleMouseDown}
       onMouseMove={handleMouseMove}
       onMouseUp={handleMouseUp}
-      style={{ width: '100vw' }}
     />
   );
 }
