4e6b6e217a992a5dc7d448c72a27dc77dfecea11
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2020-11-24 13:25:49 +0200

    Chore: Variable name cleanup in mouse position test

--- src/utils/get-mouse-pos.test.ts
+++ src/utils/get-mouse-pos.test.ts
@@ -2,17 +2,17 @@ import { MouseEvent } from 'react';
 
 import getMousePos from './get-mouse-pos';
 
-const FakeMouseEvent = ({
+const MockMouseEvent = ({
   clientX: 150,
   clientY: 50,
 } as unknown) as MouseEvent;
 
-const FakeCanvas = ({
+const MockCanvas = ({
   getBoundingClientRect: () => ({ left: 50, top: 20 }),
 } as unknown) as HTMLCanvasElement;
 
 describe('get mouse position from Canvas and a mouse event', () => {
   it('should calculate the co-ordinates offset by the position of the canvas', () => {
-    expect(getMousePos(FakeCanvas, FakeMouseEvent)).toEqual({ x: 100, y: 30 });
+    expect(getMousePos(MockCanvas, MockMouseEvent)).toEqual({ x: 100, y: 30 });
   });
 });
