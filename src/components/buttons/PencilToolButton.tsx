87daaf8d8da61cb3d4d5badf86ba764e1e592ab2
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2020-11-24 19:35:35 +0200

    Chore: Adding PencilToolButton component

---
+++ src/components/buttons/PencilToolButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FaPencilAlt } from 'react-icons/fa';
+
+function PencilToolButton(): React.ReactElement {
+  return (
+    <button>
+      <FaPencilAlt />
+    </button>
+  );
+}
+
+export default PencilToolButton;
