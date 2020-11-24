41f4158a5e587ac27692dc9361343bc3b32fe568
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2020-11-24 20:54:56 +0200

    Feature: Added context menu toolbar and added support to change the brush size

--- src/pages/Paint.tsx
+++ src/pages/Paint.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { RGBColor } from 'react-color';
+import { Slider } from '@reach/slider';
 import styled from 'styled-components';
+
+import '@reach/slider/styles.css';
 
 import Canvas from '../components/Canvas';
 import ColorPicker from '../components/ColorPicker';
@@ -14,13 +17,27 @@ const DEFAULT_RGBA = {
 };
 
 const Container = styled.div`
-  display: flex;
   border-top: solid 0.1em hsl(0, 0%, 97%);
   border-bottom: solid 0.1em hsl(0, 0%, 97%);
+
+  display: grid;
+  grid-template-areas:
+    'toolbar context-menu'
+    'toolbar canvas';
+  grid-template-columns: 6.25em auto;
 `;
 
-const ToolbarContainer = styled.div`
-  width: 6.25em;
+const ContextMenuArea = styled.div`
+  grid-area: context-menu;
+  border: 1px solid red;
+  padding: 0.5em;
+  display: flex;
+  justify-content: center;
+`;
+
+const ToolbarArea = styled.div`
+  grid-area: toolbar;
+  // width: 6.25em;
   border-right: solid 0.1em hsl(0, 0%, 97%);
   padding: 0.5em;
 
@@ -30,9 +47,8 @@ const ToolbarContainer = styled.div`
   gap: 1em;
 `;
 
-const CanvasContainer = styled.div`
-  flex-grow: 1;
-  min-width: 0;
+const CanvasArea = styled.div`
+  grid-area: canvas;
 `;
 
 function Paint(): React.ReactElement {
@@ -41,16 +57,17 @@ function Paint(): React.ReactElement {
 
   return (
     <Container>
-      <ToolbarContainer>
+      <ContextMenuArea>
+        <label>Brush Size: {brushSize}px</label>
+        <Slider min={1} max={200} value={brushSize} onChange={setBrushSize} />
+      </ContextMenuArea>
+      <ToolbarArea>
         <PencilToolButton />
         <ColorPicker color={color} onChange={({ rgb }) => setColor(rgb)} />
-        <button onClick={() => setBrushSize(brushSize + 2)}>
-          Increase by 2
-        </button>
-      </ToolbarContainer>
-      <CanvasContainer>
+      </ToolbarArea>
+      <CanvasArea>
         <Canvas color={color} brushSize={brushSize} />
-      </CanvasContainer>
+      </CanvasArea>
     </Container>
   );
 }
