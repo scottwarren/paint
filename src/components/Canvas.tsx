0b4e924d3186be0fef2157533a3a142b36a8efb9
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2020-11-25 17:12:03 +0200

    Chore: Refactored Canvas element to use Fabric
    Easier to:
    Export to JSON
    Import from JSON
    Gather statistics

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,11 +1,16 @@
-import React, { useEffect, useRef, useLayoutEffect } from 'react';
+import React, { useEffect, useLayoutEffect, useState, useRef } from 'react';
 import { RGBColor } from 'react-color';
 import styled from 'styled-components';
+import { fabric } from 'fabric';
 
-import getMousePos from '../utils/get-mouse-pos';
 import getCSSColorFromRGBColor from '../utils/get-css-color-from-rgb-color';
 
 const PaintingCanvas = styled.canvas`
+  width: 100%;
+  height: 100%;
+`;
+
+const Container = styled.div`
   width: 100%;
   height: 100%;
 `;
@@ -16,121 +21,44 @@ interface CanvasProps {
 }
 
 function Canvas({ color, brushSize }: CanvasProps): React.ReactElement {
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const [canvas, setCanvas] = useState<fabric.Canvas | null>(null);
 
-  let isMouseDown = false;
-
-  let canvas = canvasRef?.current;
-  let context = canvas?.getContext('2d');
-
-  // Any time the color changes, update the stroke of the line we're drawing
   useEffect(() => {
-    if (context) {
-      context.strokeStyle = getCSSColorFromRGBColor(color);
-    }
+    setCanvas(
+      new fabric.Canvas('paint-canvas', {
+        isDrawingMode: true,
+        freeDrawingCursor: 'round',
+        fill: getCSSColorFromRGBColor(color),
+      })
+    );
+
+    return () => {
+      if (!canvas) return;
+
+      canvas.dispose();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!canvas) return;
+
+    canvas.freeDrawingBrush.color = getCSSColorFromRGBColor(color);
+
+    setCanvas(canvas);
   }, [color]);
 
   useEffect(() => {
-    if (context) {
-      context.lineWidth = brushSize;
-    }
+    if (!canvas) return;
+
+    canvas.freeDrawingBrush.width = brushSize;
+
+    setCanvas(canvas);
   }, [brushSize]);
 
-  // Any time canvasRef changes, we need to set the canvas/context variables so we have access to the correct canvas
-  useLayoutEffect(() => {
-    canvas = canvasRef?.current;
-    context = canvas?.getContext('2d');
-  }, [canvasRef]);
-
-  useEffect(() => {
-    if (!canvas) {
-      console.log('...waiting for canvas...');
-      return;
-    }
-
-    // Setting the Canvas height/width so it scales appropriately and doesn't reset to default (300px)
-    // Required because I'm using viewport units (rather than a fixed value e.g. px)
-    canvas.setAttribute(
-      'width',
-      window.getComputedStyle(canvas, null).getPropertyValue('width')
-    );
-
-    canvas.setAttribute(
-      'height',
-      window.getComputedStyle(canvas, null).getPropertyValue('height')
-    );
-  }, [canvasRef]);
-
-  function handleMouseDown(ev: React.MouseEvent) {
-    ev.preventDefault();
-    console.log('handleMouseDown');
-    isMouseDown = true;
-
-    if (!context) {
-      console.error('no context yet...');
-      debugger;
-      return;
-    }
-
-    if (!canvas) {
-      console.error('no canvas yet...');
-      return;
-    }
-
-    context.beginPath();
-    const { x, y } = getMousePos(canvas, ev);
-
-    context.strokeStyle = getCSSColorFromRGBColor(color);
-    context.lineWidth = brushSize;
-    context.lineCap = 'round';
-    context.lineTo(x, y);
-    context.stroke();
-    context.moveTo(x, y);
-  }
-
-  function handleMouseMove(ev: React.MouseEvent) {
-    ev.preventDefault();
-    if (!context) {
-      console.error('no context yet...');
-      return;
-    }
-
-    if (!canvas) {
-      console.error('no canvas yet...');
-      return;
-    }
-
-    // The mouse being pressed indicates the user is drawing
-    if (!isMouseDown) {
-      return;
-    }
-
-    const { x, y } = getMousePos(canvas, ev);
-
-    context.lineTo(x, y);
-    context.stroke();
-    context.beginPath();
-    context.moveTo(x, y);
-  }
-
-  function handleMouseUp(ev: React.MouseEvent) {
-    ev.preventDefault();
-    isMouseDown = false;
-
-    console.log(
-      context?.getImageData(0, 0, canvas?.width ?? 0, canvas?.height ?? 0)
-    );
-
-    context?.closePath();
-  }
-
   return (
-    <PaintingCanvas
-      ref={canvasRef}
-      onMouseDown={handleMouseDown}
-      onMouseMove={handleMouseMove}
-      onMouseUp={handleMouseUp}
-    />
+    <Container>
+      <PaintingCanvas id='paint-canvas' />
+    </Container>
   );
 }
 
