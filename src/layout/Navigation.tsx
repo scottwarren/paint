5d78e33abebc94e87957cd1433c60944b6d801c1
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2020-11-25 13:43:43 +0200

    Feature: Improved the UI of the navigation :sparkles:
    Now features icons and link buttons :eyes:

--- src/layout/Navigation.tsx
+++ src/layout/Navigation.tsx
@@ -1,18 +1,41 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
+import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
+import BorderColorIcon from '@material-ui/icons/BorderColor';
+import BarChartIcon from '@material-ui/icons/BarChart';
+
+const Container = styled.nav`
+  display: flex;
+  justify-content: center;
+  gap: 1em;
+  background: hsl(0, 0, 0, 97%);
+  padding: 0.5em 0;
+`;
 
 function Navigation(): React.ReactElement {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to='/'>Paint</Link>
-        </li>
-        <li>
-          <Link to='/statistics'>Statistics</Link>
-        </li>
-      </ul>
-    </nav>
+    <Container>
+      <Link component={RouterLink} to='/'>
+        <Button
+          variant='contained'
+          color='default'
+          startIcon={<BorderColorIcon />}
+        >
+          Paint
+        </Button>
+      </Link>
+      <Link component={RouterLink} to='/statistics'>
+        <Button
+          variant='contained'
+          color='default'
+          startIcon={<BarChartIcon />}
+        >
+          Statistics
+        </Button>
+      </Link>
+    </Container>
   );
 }
 
