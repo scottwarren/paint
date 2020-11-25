3039fefb26b091d953c87cdf675738c0c95cef7a
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2020-11-25 19:20:04 +0200

    Fix: Fixed bug where color and stroke width no longer changed

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback, useRef } from 'react';
 import { RGBColor } from 'react-color';
 import { fabric } from 'fabric';
 
@@ -7,8 +7,6 @@ import getCSSColorFromRGBColor from '../utils/get-css-color-from-rgb-color';
 interface CanvasProps {
   color: RGBColor;
   brushSize: number;
-  // TODO: Figure out what type the arguments would be
-  // JSON string?
   onChange: (drawing?: string) => void;
 }
 
@@ -17,19 +15,26 @@ function Canvas({
   brushSize,
   onChange,
 }: CanvasProps): React.ReactElement {
-  let canvas: fabric.Canvas | null = null;
-
-  // TODO: Load initial drawing from state
+  const canvasRef = useRef(null);
+  const [canvas, setCanvas] = useState<fabric.Canvas>();
 
   useEffect(() => {
-    canvas = new fabric.Canvas('paint-canvas', {
+    if (!canvasRef?.current) return;
+
+    const canvas = new fabric.Canvas(canvasRef.current, {
       isDrawingMode: true,
       freeDrawingCursor: 'round',
       fill: getCSSColorFromRGBColor(color),
       width: 1022,
       height: 500,
     });
-  }, []);
+
+    setCanvas(canvas);
+  }, [canvasRef]);
+
+  // const ref = useFabric(color);
+
+  // TODO: Load initial drawing from state
 
   useEffect(() => {
     if (!canvas) return;
@@ -42,13 +47,15 @@ function Canvas({
     canvas.on('object:added', saveDrawing);
     canvas.on('object:modified', saveDrawing);
     canvas.on('object:removed', saveDrawing);
-  });
+  }, [color]);
 
   // Adjust the color of the stroke/brush when that is changed
   useEffect(() => {
     if (!canvas) return;
 
     canvas.freeDrawingBrush.color = getCSSColorFromRGBColor(color);
+
+    setCanvas(canvas);
   }, [color]);
 
   // Adjust stroke/brush size/width whenever that is changed
@@ -56,9 +63,19 @@ function Canvas({
     if (!canvas) return;
 
     canvas.freeDrawingBrush.width = brushSize;
+    setCanvas(canvas);
   }, [brushSize]);
 
-  return <canvas id='paint-canvas' />;
+  useEffect(() => {
+    if (!canvas) return;
+
+    console.log('canvas information');
+
+    console.log(canvas.freeDrawingBrush.width);
+    console.log(canvas.freeDrawingBrush.color);
+  });
+
+  return <canvas ref={canvasRef} id='paint-canvas' />;
 }
 
 export default Canvas;
